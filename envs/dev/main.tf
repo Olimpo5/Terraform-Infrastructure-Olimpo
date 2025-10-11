b44cf94fad1c22378f56fca4106e2c4f2e173e84
Yef666 <yef6rueda@outlook.com>
2025-10-11 16:29:02 -0600

    Crear los outputs y actualizar el entorno  del  cluster EC2

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -12,3 +12,10 @@ output "ecr_repository_url" {
   description = "URL del repositorio ECR."
   value       = module.olimpo_infra.ecr_repository_url
 }
+
+# envs/dev/main.tf (añadir al final)
+
+output "alb_dns_name" {
+  description = "Nombre DNS público del ALB."
+  value       = module.olimpo_infra.alb_dns_name
+}
--- modules/outputs.tf
+++ modules/outputs.tf
@@ -4,3 +4,10 @@ output "ecr_repository_url" {
   description = "La URL del repositorio de ECR para subir imágenes Docker."
   value       = aws_ecr_repository.olimpo_ecr_repo.repository_url
 }
+
+# modules/outputs.tf (añadir al final)
+
+output "alb_dns_name" {
+  description = "El nombre DNS público del Application Load Balancer."
+  value       = aws_lb.olimpo_alb.dns_name
+}
