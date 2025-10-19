62ba34b47073c925ddead87beaaa60cada979bc4
Kevin Tavera <KevTav20>
2025-10-18 19:57:47 -0600

    Remove default resource

--- modules/main.tf
+++ modules/main.tf
@@ -66,32 +66,6 @@ resource "aws_security_group" "olimpo_sg" {
   }
 }
 
-resource "aws_instance" "olimpo_app_server" {
-  ami           = "ami-0cfde0ea8edd312d4"
-  instance_type = var.aws_instance_type
-  key_name      = aws_key_pair.olimpo_app_key.key_name
-  user_data     = filebase64("${path.module}/vm/scripts/apps-install.sh")
-  vpc_security_group_ids = [
-    aws_security_group.olimpo_sg.id
-  ]
-  tags = {
-    Name = "${var.aws_server_name} - ${var.aws_environment}"
-  }
-
-  //PROVISIONERS
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("./keys/olimpo_app_key")
-      host        = self.public_ip
-    }
-    inline = [
-      "echo 'Conexión Exitosa!'"
-    ]
-  }
-}
-
 # CREACION DE UN EC2
 # En esta sentencia de abajo hacemos lo siguiente:
 # - Colocamos el id del AMI de ubuntu en la region us-east-2
